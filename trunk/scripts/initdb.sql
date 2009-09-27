152eb0ffa000f078158cf259da99456b6ba5ca72
bhupendra.aole@gmail.com <bhupendra.aole@gmail.com@3b6666d8-a9fe-11de-8882-bb5941c2e707>
2009-09-27 14:26:32 +0000

    Datastructure creation script.

---
+++ trunk/scripts/initdb.sql
@@ -0,0 +1,42 @@
+-- Database Schema 'actasap'
+-- User: apps/apps
+
+-- Create accounts table
+create table accounts (
+  account_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  account_name VARCHAR(80),
+  segment1 INTEGER,
+  segment2 INTEGER,
+  description VARCHAR(500)
+);
+
+-- Create journals table
+create table journals (
+  journal_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  journal_date DATE,
+  amount DECIMAL (10,2),
+  dr_account INTEGER,
+  cr_account INTEGER,
+  description VARCHAR(500)
+);
+
+-- Seeded Accounts
+insert into accounts (account_name, segment1, segment2) values ('Cash',1,1);
+insert into accounts (account_name, segment1, segment2) values ('Capital',3,1);
+insert into accounts (account_name, segment1, segment2) values ('Notes Payable',2,1);
+insert into accounts (account_name, segment1, segment2) values ('Prepaid Rent',1,2);
+insert into accounts (account_name, segment1, segment2) values ('Equipment',1,3);
+insert into accounts (account_name, segment1, segment2) values ('Accounts Receivable',1,4);
+insert into accounts (account_name, segment1, segment2) values ('Service Revenue',3,2);
+insert into accounts (account_name, segment1, segment2) values ('Unearned Revenue',2,2);
+insert into accounts (account_name, segment1, segment2) values ('Salary Expense',3,3);
+insert into accounts (account_name, segment1, segment2) values ('Utility Expense',3,4);
+insert into accounts (account_name, segment1, segment2) values ('Inventory',1,5);
+insert into accounts (account_name, segment1, segment2) values ('Sales Revenue',3,5);
+insert into accounts (account_name, segment1, segment2) values ('Interest Payable',2,3);
+insert into accounts (account_name, segment1, segment2) values ('Rent Expense',3,6);
+insert into accounts (account_name, segment1, segment2) values ('Depreciation Expense',3,7);
+insert into accounts (account_name, segment1, segment2) values ('Accounts Payable',2,3);
+insert into accounts (account_name, segment1, segment2) values ('Utilites Payable',3,8);
+insert into accounts (account_name, segment1, segment2) values ('Cost of Goods',3,9);
+insert into accounts (account_name, segment1, segment2) values ('Interest Expense',3,10);
--- trunk/src/test.java
+++
@@ -1,5 +0,0 @@
-public class test {
-	public static void main (String args[]) {
-		System.out.println("Code change in trunk after new tag!");
-	}
-}
