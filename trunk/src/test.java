f98fad06e32d567e576a0f082ec05e76ccb5ba6a
bhupendra.aole@gmail.com <bhupendra.aole@gmail.com@3b6666d8-a9fe-11de-8882-bb5941c2e707>
2009-09-27 11:56:55 +0000

    Code change in trunk after new tag!

--- trunk/src/test.java
+++ trunk/src/test.java
@@ -1,5 +1,5 @@
 public class test {
 	public static void main (String args[]) {
-		System.out.println("Creating new Tag: Accounting101!");
+		System.out.println("Code change in trunk after new tag!");
 	}
 }
