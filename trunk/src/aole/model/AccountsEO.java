bde5cd2bd1165a68a21c188c148971f760f6b807
bhupendra.aole@gmail.com <bhupendra.aole@gmail.com@3b6666d8-a9fe-11de-8882-bb5941c2e707>
2009-09-28 15:14:19 +0000

    Issue 3: Create 'Account Creation' Form

---
+++ trunk/src/aole/model/AccountsEO.java
@@ -0,0 +1,50 @@
+/*
+Accounting101
+Copyright (C) 2009  Bhupendra Aole
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+/*
+ * File: AccountsEO.java
+ * Author: Bhupendra Aole
+ * Date: Sep 28, 2009: 
+ * 
+ * Revisions:
+ * ----------
+ */
+
+package aole.model;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import aole.db.DBConnection;
+
+/**
+ * 
+ */
+public class AccountsEO {
+	public static void insertRow (String name, int seg1, int seg2,
+			String descrip) {
+		Connection con = DBConnection.getConnection();
+		try {
+			Statement stmt = con.createStatement();
+			String query = "insert into accounts (account_name, segment1, segment2, description) values ";
+			query = query + "('" + name + "'," + seg1 + "," + seg2 + ",'"
+					+ descrip + "')";
+			System.out.println(query);
+			stmt.executeUpdate(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
